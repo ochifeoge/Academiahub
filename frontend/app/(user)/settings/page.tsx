8eca46cc5591dc619e6edf0d816cd2fc3b2cbd47
Mustaphamexy <mustaphamexy111@gmail.com>
2026-03-26 08:24:56 +0000

    refactor(settings): remove profileData prop

--- frontend/app/(user)/settings/page.tsx
+++ frontend/app/(user)/settings/page.tsx
@@ -39,7 +39,7 @@ const Page = async () => {
     <main className="bg-white p-2 my-2 rounded md:mx-4 md:my-4 md:p-6 md:rounded-2xl">
       <SettingsHeader />
       <div className="lg:px-22.75 h-full md:space-y-20 py-4 lg:py-6">
-        <AllForms profileData={{ name, image, bio }} />
+        <AllForms  />
       </div>
     </main>
   );
